1a06584bb5b4fdfd9cab2212f4670c76c40ec6cc
studywithyou <studytime@lalit.netii.net>
2015-01-18 19:24:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<META http-equiv="refresh" content="0;URL=http://www.wefriends.net">
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<META http-equiv="refresh" content="0;URL="www.wefriends.net">
+
+</head>
+
+<body>
+
+<div>
+  <div class="content">
+    <h1><a href="www.wefriends.net" title="www.wefriends.net">Welcome to wefriends.net    </a><!-- end .content --></h1>
+  </div>
+<!-- end .container --></div>
+</body>
+</html>
